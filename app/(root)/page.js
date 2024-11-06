b5eeb7534d8e9adf21ee0d7776f7d7089bd7a179
abdulahad0110 <abdulahad200325@gmail.com>
2024-11-07 01:31:53 +0500

    first commit

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,101 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="-z-10">
+      <Image
+        src="https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Beautiful landscape"
+        className="w-full brightness-50 relative -z-10 object-cover"
+        width={1476}
+        height={987}
+      />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="absolute p-6 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+        <div className="text-gray-200 flex justify-center gap-4 mb-4 text-center mx-auto">
+          <span>5 Beds</span>
+          <span>2 Baths</span>
+          <span>180 sqft</span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <h1 className="text-6xl font-bold mb-8 text-white text-center leading-tight tracking-wide">
+          Office Space at<br />Northwest
+        </h1>
+        <h1 className="text-lg text-white font-medium mb-6 text-center">
+          $250{' '}
+          <span className="text-sm">/month</span>
+        </h1>
+        <div className="flex justify-center p-2">
+          <Button className="bg-accent text-black font-medium hover:cursor-pointer">
+            View Details <ArrowRight size={20} />
+          </Button>
+        </div>
+      </div>
     </div>
+    
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,51 @@
+import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { ChevronDown, Phone } from 'lucide-react'
 
 function Navbar() {
   return (
-    <div className=" mx-auto flex justify-between">
+    <div className="w-full py-5 shadow-md bg-transparent fixed z-20 text-white">
+      <div className="md:w-11/12 mx-auto flex justify-between items-center">
 
-      <div className="flex">
-        <a className="btn btn-ghost text-xl font-mono">Real State</a>
-      </div>
+        <div className="flex">
+          <h1 className="text-xl font-mono">Real State</h1>
+        </div>
 
-      <div className="flex">
-        <a>Link</a>
-        <a>Link</a>
-        <a>Link</a>
-        <a>Link</a>
-      </div>
+        <div className=" md:block none">
+          <Button variant="link" asChild>
+            <Link href={'/'} className="flex items-center text-white">Home<ChevronDown /></Link>
+          </Button>
+          <Button variant="link" asChild>
+            <Link href={'/'} className="flex items-center text-white">Members<ChevronDown /></Link>
+          </Button>
+          <Button variant="link" asChild>
+            <Link href={'/'} className="flex items-center text-white">Blog<ChevronDown /></Link>
+          </Button>
+          <Button variant="link" asChild>
+            <Link href={'/'} className="flex items-center text-white">Pages<ChevronDown /></Link>
+          </Button>
+          <Button variant="link" asChild>
+            <Link href={'/'} className="flex items-center text-white">Contact</Link>
+          </Button>
+        </div>
 
-      <div className="flex">
-        <a>Link</a>
-        <a>Link</a>
+        <div className="flex items-center gap-3">
+          <div className="flex items-center">
+            <Phone size={20} />
+            <span className="ml-1">+92 330 2355684</span>
+          </div>
+
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+
+          <Button variant="outline" className="rounded-full border-2 border-black hover:text-white hover:bg-black transition-colors" asChild>
+            <Link href="/login" className='text-black'>Login</Link>
+          </Button>
+        </div>
       </div>
     </div>
   )
